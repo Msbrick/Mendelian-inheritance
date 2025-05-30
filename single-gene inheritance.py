4557f1523b132112003193cadc4a8d227e64f14c
Msbrick <mycloud171@outlook.com>
2025-05-30 20:43:36 +0900

    Update single-gene inheritance.py

--- single-gene inheritance.py
+++ single-gene inheritance.py
@@ -36,9 +36,25 @@ def father_phenotype(*phenotype) :
 dominate = ["A", "B", "C", "D", "E", "F","G","H","I","J","K","L","N","M","O","P","Q","R","T","U","V","W","X","Y","Z"]
 recessive = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+a_c_list = []
+a = 
 for i in dominate:
   for j in recessive:
-    
+    d = i + j
+    d = j + i
+v = input()
+h = input()
+
+if v != h and v == "R" or h == "r" :
+    e = [v + h, h + v]
+elif v == h and v == "R" or h == "r" :
+    e = [h + v]
+if v != h:
+  percentage = (a_c_list.count(h + v) + a_c_list.count(v + h)) / len(a_c_list) * 100 #c_genetic_trait에서 입력받은 형질 / 나올 수 있는 전체형질
+elif v == h:
+  percentage = a_c_list.count(h + v) / len(a_c_list) * 100
+
+
   
 
 
