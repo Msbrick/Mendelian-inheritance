833c4130901fedb200cf536ccdf53fee12c14e4e
Msbrick <mycloud171@outlook.com>
2025-05-30 20:36:12 +0900

    single-gene inheritance.py

--- single-gene inheritance.py
+++ single-gene inheritance.py
@@ -1,19 +1,39 @@
 print("대립형질끼리 순서대로 입력하시오")
 
-def mother(*phenotype):
+def mother_phenotype(*phenotype) :
   dominate = ["A", "B", "C", "D", "E", "F","G","H","I","J","K","L","N","M","O","P","Q","R","T","U","V","W","X","Y","Z"]
   recessive = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   phenotype = [phenotype]
   L = len(phenotype)
   L1 = L - 1
-  for I in range(0:L):
+  for i in range(0:L):
     for j in range(0:L1):
       i = 0
       j = i + 1
       a = phenotype[i: j]
       b = "phenotype{j}"
       c = c[0] + c[1]
-      all_phenotype = [c] 
+      all_phenotype = [c]
+      result = all_phenotype
+      return result
+
+def father_phenotype(*phenotype) :
+  dominate = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "N", "M", "O", "P", "Q", "R", "T", "U", "V", "W", "X", "Y", "Z"]
+  recessive = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  phenotype = [phenotype]
+  L = len(phenotype)
+  L1 = L - 1
+  for i in range(0:L):
+    for j in range(0:L1):
+      i = 0
+      j = i + 1
+      a = phenotype[i: j]
+      b = "phenotype{j}"
+      c = c[0] + c[1]
+      all_phenotype = [c]
+      result = all_phenotype
+      return result
+  
 
 
 
