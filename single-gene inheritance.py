f663b544aa4e6e435edc0ad1361c6453bff92e9e
Msbrick <mycloud171@outlook.com>
2025-05-30 20:57:49 +0900

    Update single-gene inheritance.py

--- single-gene inheritance.py
+++ single-gene inheritance.py
@@ -32,7 +32,21 @@ def father_phenotype(*phenotype) :
 dominate = ["A", "B", "C", "D", "E", "F","G","H","I","J","K","L","N","M","O","P","Q","R","T","U","V","W","X","Y","Z"]
 recessive = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-all_phenotype = list(zip(
+all_phenotype = list(zip(["A", "B", "C", "D", "E", "F","G","H","I","J","K","L","N","M","O","P","Q","R","T","U","V","W","X","Y","Z"], ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']))
+  
+phenotype[phenotype]
+L = len(phenotype)
+L1 = L - 1
+  for I in range(0:L):
+  for j in range(0:L1):
+      i = 0
+      j = i + 1
+      a = phenotype[i: j]
+      b = "phenotype{j}"
+      c = c[0] + c[1]
+      all_phenotype = [c]
+      result = all_phenotype
+      return result
     
 v = input()
 h = input()
