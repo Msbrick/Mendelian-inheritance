8cb209d3eabbbd3e861a1ab7b5827c231451a8ed
Msbrick <mycloud171@outlook.com>
2025-05-30 20:23:09 +0900

    single-gene inheritance.py

--- single-gene inheritance.py
+++ single-gene inheritance.py
@@ -1,3 +1,26 @@
 print("대립형질끼리 순서대로 입력하시오")
 
-def mother(phenotype)
+def mother(*phenotype):
+  dominate = ["A", "B", "C", "D", "E", "F","G","H","I","J","K","L","N","M","O","P","Q","R","T","U","V","W","X","Y","Z"]
+  recessive = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  phenotype = [phenotype]
+  L = len(phenotype)
+  L1 = L - 1
+  for I in range(0:L):
+    for j in range(0:L1):
+      i = 0
+      j = i + 1
+      a = phenotype[I: j]
+      b = "phenotype{j}"
+
+
+      c = str(b)
+
+
+
+
+      
+
+
+
+
