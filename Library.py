cb860146e973bc63c0ff0cd478d08d8140d1596b
Msbrick <mycloud171@outlook.com>
2025-06-02 20:05:55 +0900

    Library.py

--- Library.py
+++ Library.py
@@ -1,26 +1,10 @@
-def single-factor inheritance_2_a.m(a,b):
-  m_list = []
-  if ord(a) - ord(b) == 32 or ord(a) - ord(b) == -32 or ord(a) - ord(b) == 0 :
-    c = a + b
-    m_list.append(c)
-  result = m_list
-  return result
-
-def single-factor inheritance_2_a.f(c,d):
-  m_list = []
-  if ord(a) - ord(b) == 32 or ord(a) - ord(b) == -32 or ord(a) - ord(b) == 0 :
-    c = a + b
-    m_list.append(c)
-  result = m_list
-  return result
-
-def single_factor_inheritance_m(a_s, *phenotype) :
-  m_list = [phenotype]
+def single_factor_inheritance_m(a_s, *phenotype1) :
+  m_list = [phenotype1]
   M = m_list
   a_m_list = []
   if a_s == "autosomal" :
-      for i in phenotype :
-        for j in phenotype :
+      for i in phenotype1 :
+        for j in phenotype1 :
           if ord(i) - ord(j) == 0 :
             k = i + j   
             a_m_list.append(k)
@@ -32,13 +16,13 @@ def single_factor_inheritance_m(a_s, *phenotype) :
   result = a_m_list
   return result
 
-def single_factor_inheritance_f(a_s, *phenotype) :
-  f_list = [phenotype]
+def single_factor_inheritance_f(a_s, *phenotype2) :
+  f_list = [phenotype2]
   F= f_list
   a_f_list = []
   if a_s == "autosomal" :
-      for i in phenotype :
-        for j in phenotype :
+      for i in phenotype2 :
+        for j in phenotype2 :
           if ord(i) - ord(j) == 0 :
             k = i + j   
             a_f_list.append(k)
