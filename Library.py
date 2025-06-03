9ccfba05a4e704bf86435b28f8c43adc6f5ab4bd
Msbrick <mycloud171@outlook.com>
2025-06-03 15:52:10 +0900

    Update Library.py

--- Library.py
+++ Library.py
@@ -2,35 +2,30 @@ def single_factor_inheritance_m(a_s, *phenotype1) :
   m_list = [phenotype1]
   M = m_list
   a_m_list = []
+  A_m_list = []
   if a_s == "autosomal" :
-      for i in phenotype1 :
-        for j in phenotype1 :
-          if ord(i) - ord(j) == 0 :
-            k = i + j   
-            a_m_list.append(k)
-          elif ord(i) - ord(j) ==32 :
-            k = i + j
-            N = j + i
-            a_m_list.append(k)
-            a_m_list.append(N)
-  result = a_m_list
+    for i in phenotype1 :
+      for j in phenotype1 :
+        if ord(i) - ord(j) == 0 or ord(i) - ord(j) == 32 or ord(i) - ord(j) == -32 :
+          k = i + j
+          a_m_list.append(k)
+          print(a_m_list)
+          L = len(a_m_list)
+    for i in range(0, L):
+      i = 0
+      i = i + 1
+      if i % 4 == 0 :
+        S = a_m_list[i:i+4]
+        print(S)
+        for x in S:
+          for y in S:
+            for z in S:
+              A_m_list.append(x)
+              A_m_list.append(y)
+              A_m_list.append(z)
+           
+  result = A_m_list
   return result
 
-def single_factor_inheritance_f(a_s, *phenotype2) :
-  f_list = [phenotype2]
-  F= f_list
-  a_f_list = []
-  if a_s == "autosomal" :
-      for i in phenotype2 :
-        for j in phenotype2 :
-          if ord(i) - ord(j) == 0 :
-            k = i + j   
-            a_f_list.append(k)
-          elif ord(i) - ord(j) ==32 :
-            k = i + j
-            N = j + i
-            a_f_list.append(k)
-            a_f_list.append(N)
-  result = a_f_list
-  return result
 
+print(single_factor_inheritance_m('autosomal', input(), input(),input(), input(), input(), input()))
