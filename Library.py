393fc126f94ad8d7764c611e68b76f8b897e88e2
Msbrick <mycloud171@outlook.com>
2025-06-10 22:34:30 +0900

    single-gene inheritance_Library.py

--- Library.py
+++ Library.py
@@ -1,31 +1,45 @@
-def single_factor_inheritance_m(a_s, *phenotype1) :
-  m_list = [phenotype1]
-  M = m_list
-  a_m_list = []
-  A_m_list = []
-  if a_s == "autosomal" :
-    for i in phenotype1 :
-      for j in phenotype1 :
-        if ord(i) - ord(j) == 0 or ord(i) - ord(j) == 32 or ord(i) - ord(j) == -32 :
-          k = i + j
-          a_m_list.append(k)
-          print(a_m_list)
-          L = len(a_m_list)
-    for i in range(0, L):
-      i = 0
-      i = i + 1
-      if i % 4 == 0 :
-        S = a_m_list[i:i+4]
-        print(S)
-        for x in S:
-          for y in S:
-            for z in S:
-              A_m_list.append(x)
-              A_m_list.append(y)
-              A_m_list.append(z)
-           
-  result = A_m_list
-  return result
+def p_genetic_trait_a_i(phenotype_m, phenotype_f) :
+    dominate = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    recessive = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    m_list = list(phenotype_m)
+    f_list = list(phenotype_f)
+    print(m_list)
+    p_list = m_list + f_list
+    m_p_list = list()
+    f_p_list = list()
+    L = len(f_list)
+    a_p_list = list()
+    a_c_list = list()
+    p_f_list = list()
+    nny_list = list()
+    L1 = len(m_list) *2
+    for idx in range(0
+                     , len(m_list), 2):
+        m_p_list.append([m_list[idx], m_list[idx+1]])
+        f_p_list.append([f_list[idx], f_list[idx+1]])
+        L = len(m_p_list)
+        for jdx in range(0,L):
+            for mdx in m_p_list[jdx]:
+                for fdx in f_p_list[jdx]:
+                    a_p_list.append(mdx + fdx)
+                    L2 = len(a_p_list) -1
+        nny_list = list(range(0,L,1))
+        a = sum(nny_list)
+        a_c_list = a_p_list[a*4: L2 + 1]
+        print(a_c_list)
+    result = a_c_list
+    return result
 
+def c_genetic_trait_a_i(phenotype) :
+    dominate = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    recessive = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    c_list = list(phenotype)
+    c_p_list = list()
+    for idx in range(0, len(c_list), 2):
+        c_p_list.append(c_list[idx] + c_list[idx+1])
+        c_p_list.append(c_list[idx + 1] + c_list[idx])
+        print(c_p_list)
+    result = c_p_list
+    return result
 
 print(single_factor_inheritance_m('autosomal', input(), input(),input(), input(), input(), input()))
